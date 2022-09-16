f492c062cbf0ed0afeebf3f14919a0fe2e58a0cb
victor gonzalez <v.gonzalezgomez.vg@gmail.com>
2022-09-16 00:05:02 -0300

    adding loading page and fixing order by index

--- src/components/PokemonTable.jsx
+++ src/components/PokemonTable.jsx
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import Table from 'react-bootstrap/Table';
-import {Card} from "react-bootstrap";
-import {FaSortAlphaDown, FaSortAlphaUpAlt} from "react-icons/fa";
+import {Badge, Card} from "react-bootstrap";
+import {FaSortAlphaDown, FaSortAlphaUpAlt, FaSortNumericDown, FaSortNumericDownAlt} from "react-icons/fa";
 
-function PokemonTable({pokemonData, handlerFilterAToZ, handlerFilterZtoA}) {
+function PokemonTable({pokemonData, handlerFilterAToZ, handlerFilterZtoA, handlerFilter1To9, handlerFilter9to1}) {
 
   const [isFilterAZ, setIsFilterAZ] = useState(true);
+  const [isFilter1to9, setIsFilter1to9] = useState(false);
+
   const onHandleAZ = () => {
     setIsFilterAZ(false)
     handlerFilterAToZ();
@@ -13,6 +15,14 @@ function PokemonTable({pokemonData, handlerFilterAToZ, handlerFilterZtoA}) {
   const onHandleZA = () => {
     setIsFilterAZ(true)
     handlerFilterZtoA();
+  }
+  const onHandle1to9 = () => {
+    setIsFilter1to9(false)
+    handlerFilter1To9();
+  }
+  const onHandle9to1 = () => {
+    setIsFilter1to9(true)
+    handlerFilter9to1();
   }
 
   return (
@@ -24,34 +34,51 @@ function PokemonTable({pokemonData, handlerFilterAToZ, handlerFilterZtoA}) {
         width: '70rem',
         height: '500px'
       }}>
-      <Card.Header>Pokemon's</Card.Header>
-
-        <thead className="text-center" >
-        <tr>
-          <th>Index</th>
-          <th>Image</th>
-          <th>Name {isFilterAZ ? <FaSortAlphaDown onClick={() => onHandleAZ()}/> :
-            <FaSortAlphaUpAlt onClick={() => onHandleZA()}/>} </th>
-          <th>Types</th>
-        </tr>
-        </thead>
-
+      <Card.Header>
+        <Badge bg="primary">Pokelist</Badge>
+        <Badge bg="dark">.</Badge>
+        <Badge bg="warning">.</Badge>
+        <Badge bg="success">.</Badge>
+       </Card.Header>
       <Card.Body>
         <div style={{
           height: ' 25rem',
-          overflow: 'scroll'
-        }}>
-          <Table striped bordered hover variant="dark">
+          overflow: 'scroll',
+          overflowX: 'hidden'
+        }}
+             id="customTable"
+        >
 
-            <tbody className="justify-content-center">
+          <Table striped bordered hover variant="dark" bg="dark" className="nes-container">
+            <thead className="text-center" style={{
+              position: 'sticky',
+              top: '0'
+            }}>
+            <tr>
+              <th style={{
+                position: 'sticky',
+                top: '0'
+              }}>Index {isFilter1to9 ? <FaSortNumericDownAlt onClick={() => onHandle1to9()}/> :
+                <FaSortNumericDown onClick={() => onHandle9to1()}/>}</th>
+              <th style={{
+                position: 'sticky',
+                top: '0'
+              }}>Image
+              </th>
+              <th style={{
+                position: 'sticky',
+                top: '0'
+              }}>Name {isFilterAZ ? <FaSortAlphaDown onClick={() => onHandleAZ()}/> :
+                <FaSortAlphaUpAlt onClick={() => onHandleZA()}/>} </th>
+
+            </tr>
+            </thead>
+            <tbody>
             {pokemonData.map((pokemon, index) => (
               <tr key={index}>
                 <td>#{pokemon.id}</td>
                 <td><img src={pokemon.sprites.front_default}/></td>
                 <td>{pokemon.name}</td>
-                <td>{pokemon.types.map((type, index) => {
-                  return <li key={index}>{type.type.name}</li>
-                })}</td>
               </tr>
             ))
             }
